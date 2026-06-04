a91c339b3ec42c074cc6b9ada0136b9db102eb6b
serifgungor <gungoronline@gmail.com>
2026-06-04 20:18:01 +0300

    fix

--- app/src/main/java/com/timas/superapp/components/SuperAppToolbar.kt
+++ app/src/main/java/com/timas/superapp/components/SuperAppToolbar.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
